5c2a7ff05a80e004877074e1d70100becb86261f
Azret19 <138998249+Azret19@users.noreply.github.com>
2023-08-15 12:24:38 +0600

    homeworks

--- JavaScript_3/javascript-project/js/home_works.js
+++ JavaScript_3/javascript-project/js/home_works.js
@@ -16,15 +16,102 @@ gmailCheck.onclick = () => {
 }
 
 // const parentBlock = document.querySelector('.parent_block')
-const childBlock = document.querySelector('.child_block')
-let moveBlock = 0
-const block = () => {
-    if (moveBlock < 447) {
-        moveBlock++
-        childBlock.style.left =`${moveBlock}px`
-        block()
+const box = document.querySelector('.child_block')
+
+let PositionX = 0
+let PositionY = 0
+
+const move = () => {
+    if (PositionX < 448 && PositionY === 0) {
+        PositionX++
+        box.style.left = `${PositionX}px`
+        setTimeout(move, 1)
+    } else if (PositionX >= 448 && PositionY < 448) {
+        PositionY++
+        box.style.top = `${PositionY}px`
+        setTimeout(move, 1)
+    }else if (PositionY > 0 && PositionX > 0) {
+        PositionX--
+        box.style.left = `${PositionX}px`
+        setTimeout(move, 1)
+    }else if (PositionX === 0 && PositionY > 0) {
+        PositionY--
+        box.style.top = `${PositionY}px`
+        setTimeout(move, 1)
+    }
+
+}
+move()
+
+const seconds = document.querySelector('#seconds')
+const buttonStart = document.querySelector('#start')
+const buttonStop = document.querySelector('#stop')
+const buttonReset = document.querySelector('#reset')
+const buttonResume = document.querySelector('#resume')
+
+let secondsTime = 0
+let interval
+
+const startTime = () => {
+  secondsTime++
+  seconds.innerHTML = secondsTime
+}
+
+buttonStart.onclick = () => {
+    clearInterval(interval)
+    interval = setInterval(startTime, 1000)
+    buttonStart.onclick = buttonStop
+    buttonStop.onclick = () => {
+        clearInterval(interval)
+        buttonResume.onclick = () => {
+            clearInterval(interval)
+            interval = setInterval(startTime, 1000)
+            buttonResume.onclick = buttonStop
+        }
+    }
+    buttonReset.onclick = () => {
+        clearInterval(interval)
+        seconds.innerHTML = '0'
+        secondsTime = 0
+        buttonResume.onclick = () => {
+            clearInterval(interval)
+        }
+        buttonStart.onclick = () => {
+            clearInterval(interval)
+            interval = setInterval(startTime, 1000)
+            buttonStart.onclick = buttonStart
+        }
     }
 }
-block()
-// [a-zA-Z][a-zA-Z0-9\-\_\.]
-// parentBlock.style.left = childBlock+"px"
+
+
+
+
+
+
+// buttonReset.onclick = () => {
+//     seconds.innerHTML = '0'
+//     secondsTime = 0
+//     clearInterval(interval)
+//             buttonResume.onclick = () => {
+//                 clearInterval(interval)
+//                 interval = setInterval(startTime, 1000)
+//                 buttonStart.onclick = buttonStop
+//     }
+// }
+
+
+
+// buttonStart.onclick = () => {
+//     clearInterval(interval)
+//     interval = setInterval(startTime, 1000)
+//     buttonStart.onclick = buttonStop
+//     buttonStop.onclick = () => {
+//         clearInterval(interval)
+//         buttonResume.onclick = () => {
+//             clearInterval(interval)
+//             interval = setInterval(startTime, 1000)
+//             buttonReset.onclick = buttonStop
+//         }
+//     }
+// }
---
+++ JavaScript_3/lesson_2/app.js
@@ -0,0 +1,39 @@
+
+// Event loop - Цикл событий
+// Основы асинхронности
+
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+
+// setTimeout(() => {
+//     console.log(1)
+// }, 0)
+//
+// setTimeout(() => {
+//     console.log(2)
+// }, 0)
+//
+// console.log(3)
+//
+// setTimeout(() => {
+//     console.log(4)
+// }, 0)
+
+
+// const interval = setInterval(() => {
+//     console.log('Hello')
+// }, 1000)
+//
+// setTimeout(() => {
+//     clearInterval(interval)
+// }, 5000)
+
+// const logger = () => console.log('LOG!') // lambda function
+//
+// setTimeout(logger, 3000)
+
+// Event loop - Цикл событий
+
