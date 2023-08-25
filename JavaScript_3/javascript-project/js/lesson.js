9cdf9b61b87a47ca8be2f93e5abeb0e404e12c75
Azret19 <138998249+Azret19@users.noreply.github.com>
2023-08-25 21:26:41 +0600

    homeworks

--- JavaScript_3/javascript-project/js/lesson.js
+++ JavaScript_3/javascript-project/js/lesson.js
@@ -65,3 +65,103 @@ const autoSlider = (i = 0) => {
 }
 
 autoSlider(index)
+
+//CONVERTER
+
+
+const som = document.querySelector('#som')
+const usd = document.querySelector('#usd')
+const eur = document.querySelector('#eur')
+const convert = (element, target) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest()
+        request.open("GET", "../data/converter.json")
+        request.setRequestHeader("Content-type", "application/json")
+        request.send()
+        request.onload = () => {
+            const response = JSON.parse(request.response)
+            target.forEach( e =>{
+                if (e === som) {
+                    e.value = (element.value * response[element.id]).toFixed(2)
+                } else {
+                    e.value = ((element.value * response[element.id]) / response[e.id]).toFixed(2)
+                }
+            })
+            if (element.value === '') {
+                target.forEach(e => e.value = '')
+            }
+
+            element.value === '' && (target.forEach(e => e.value = ''))
+        }
+    }
+}
+convert(som,[usd,eur])
+convert(usd,[som,eur])
+convert(eur,[som,usd])
+
+
+
+
+
+
+
+
+
+
+
+// if (target2 === 'som') {
+//     e.value = (elem.value / response[e.id]).toFixed(2)
+// }
+// else
+
+// const som = document.querySelector('#som')
+// const usd = document.querySelector('#usd')
+// const eur = document.querySelector('#eur')
+//
+//
+// const converter = (element, target, target2, isTrue) => {
+//     element.oninput= () => {
+//         const request = new XMLHttpRequest()
+//         request.open("GET", "../data/converter.json")
+//         request.setRequestHeader("Content-type", "application/json")
+//         request.send()
+//         request.onload = () => {
+//         const response = JSON.parse(request.response)
+//             if (isTrue) {
+//                 target.value = (element.value / response.usd).toFixed(2)
+//                 target2.value = (element.value / response.eur).toFixed(2)
+//                 //Конвертер сома
+//             }
+//             else if (isTrue) {
+//                 target.value = (element.value * response.eur).toFixed(2)
+//                 target2.value = ((element.value * response.eur) / response.usd).toFixed(2)
+//                 //Конвертер евро
+//             }
+//             else {
+//                 target.value = (element.value * response.usd).toFixed(2)
+//                 target2.value = ((element.value * response.usd) / response.eur).toFixed(2)
+//                 //Конвертер доллара
+//             }
+//                 element.value === '' && (target.value = '')
+//                 element.value === '' && (target2.value = '')
+//         }
+//     }
+// }
+// converter(som, usd, eur,true)
+// converter(eur, som, usd,false)
+// converter(usd, som, eur,false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
